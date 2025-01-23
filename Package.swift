15365de1bf8e7db303a65b46de23d29fde0d8a00
Andrey Sak <andrei.sak@banuba.com>
2025-01-23 17:41:16 +0300

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,23 +5,30 @@ import PackageDescription
 let utilsVersionRange: Range<Version> = "1.40.0"..<"1.49.99"
 
 let package = Package(
-  name: "BanubaVideoEditorCore",
-  platforms: [
-    .iOS(.v15)
-  ],
-  products: [
-    .library(
-      name: "BanubaVideoEditorCore",
-      targets: ["BanubaVideoEditorCore"]
-    )
-  ],
-  dependencies: [
-    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", utilsVersionRange),
-  ],
-  targets: [
-    .binaryTarget(
-      name: "BanubaVideoEditorCore",
-      path: "BanubaVideoEditorCore.xcframework"
-    )
-  ]
+    name: "BanubaVideoEditorCore",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "BanubaVideoEditorCore",
+            targets: [
+                "BanubaVideoEditorCore",
+                "BanubaUtilities"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", utilsVersionRange),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BanubaVideoEditorCore",
+            path: "BanubaVideoEditorCore.xcframework"
+        ),
+        .binaryTarget(
+            name: "BanubaUtilities",
+            path: "BanubaUtilities.xcframework"
+        ),
+    ]
 )
