e56c9ee49079db12422433056ccf3c4f679bbecc
CI <ci@banuba.com>
2025-01-30 13:37:12 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,43 +2,41 @@
 
 import PackageDescription
 
-let utilsVersionRange: Range<Version> = "1.40.0"..<"1.49.99"
+let version: Version = "1.40.1"
 
 let package = Package(
-    name: "BanubaVideoEditorCore",
-    platforms: [
-        .iOS(.v15)
-    ],
-    products: [
-        .library(
-            name: "BanubaVideoEditorCore",
-            targets: [
-                "BanubaVideoEditorCoreTarget",
-            ]
-        )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", utilsVersionRange),
-    ],
-    targets: [
-        .target(
-          name: "BanubaVideoEditorCoreWrapper",
-          dependencies: [
-            "BanubaVideoEditorCore",
-            .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS")
-          ],
-          path: "BanubaVideoEditorCoreWrapper"
-        ),
-        .target(
-          name: "BanubaVideoEditorCoreTarget",
-          dependencies: [
-            .target(name: "BanubaVideoEditorCoreWrapper")
-          ],
-          path: "BanubaVideoEditorCoreTarget"
-        ),
-        .binaryTarget(
-          name: "BanubaVideoEditorCore",
-          path: "BanubaVideoEditorCore.xcframework"
-        )
-    ]
+  name: "BanubaVideoEditorCore",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    .library(
+      name: "BanubaVideoEditorCore",
+      targets: ["BanubaVideoEditorCoreTarget"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
+  ],
+  targets: [
+    .target(
+      name: "BanubaVideoEditorCoreWrapper",
+      dependencies: [
+        "BanubaVideoEditorCore",
+        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS")
+      ],
+      path: "BanubaVideoEditorCoreWrapper"
+    ),
+    .target(
+      name: "BanubaVideoEditorCoreTarget",
+      dependencies: [
+        .target(name: "BanubaVideoEditorCoreWrapper")
+      ],
+      path: "BanubaVideoEditorCoreTarget"
+    ),
+    .binaryTarget(
+      name: "BanubaVideoEditorCore",
+      path: "BanubaVideoEditorCore.xcframework"
+    )
+  ]
 )
