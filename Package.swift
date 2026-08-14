64bf7023c03ea04686ed8bfc606ee425a3ec04ba
CI <ci@banuba.com>
2026-08-14 16:51:45 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,7 +2,8 @@
 
 import PackageDescription
 
-let banubaUtilsVersionRange: Range<Version> = "1.53.1"..<"1.53.99"
+let banubaUtilsVersionRange: Range<Version> = "1.54.0"..<"1.59.99"
+let banubaLicenseServicingVersionRange: Range<Version> = "1.54.0"..<"1.59.99"
 
 let package = Package(
   name: "BanubaVideoEditorCore",
@@ -17,13 +18,15 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", banubaUtilsVersionRange),
+    .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", banubaLicenseServicingVersionRange),
   ],
   targets: [
     .target(
       name: "BanubaVideoEditorCoreWrapper",
       dependencies: [
         "BanubaVideoEditorCore",
-        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS")
+        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS"),
+        .product(name: "BanubaLicenseServicingSDK", package: "BanubaLicenseServicingSDK")
       ],
       path: "BanubaVideoEditorCoreWrapper"
     ),
@@ -36,8 +39,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "BanubaVideoEditorCore",
-      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaVideoEditorCore/1.53.1/BanubaVideoEditorCore-1.53.1.xcframework.zip",
-      checksum: "6886a55aacbdc5420d3411a0f199c3860fe4f3d3c733105f72fa2eda54975cea"
+      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaVideoEditorCore/1.54.0/BanubaVideoEditorCore-1.54.0.xcframework.zip",
+      checksum: "a6f00d63ac7ada233cac2118b5218a71f100064e1b708a71625db368286e338d"
     )
   ]
 )
